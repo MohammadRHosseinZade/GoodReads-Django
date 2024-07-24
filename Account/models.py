58085af3f988251dfff55f679cc068647f822801
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-24 14:12:24 +0330

    added custom user model

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(verbose_name="username", max_length=128, unique=True, db_index=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username 
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    @is_staff.setter
+    def is_staff(self, value):
+        self.is_admin = value
+    
+    def has_module_perms(self, app_label):
+       return self.is_admin
+    
+    def has_perm(self, perm, obj=None):
+       return self.is_admin
+    
