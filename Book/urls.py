dc3103b389e1a2a0be82b5682ea9aeba37037de9
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-25 20:34:20 +0330

    added recomendation endpoint

--- Book/urls.py
+++ Book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookListCreateView, BookRetrieveUpdateDestroyView, BooksByGenreView
+from .views import BookListCreateView, BookRetrieveUpdateDestroyView, BooksByGenreView, RecommendedBooksView
 
 urlpatterns = [
     path('', BookListCreateView.as_view(), name='book-list-create'),
     path('<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
     path('by-genre/', BooksByGenreView.as_view(), name='books-by-genre'),
+    path('recommendations/', RecommendedBooksView.as_view(), name='recommended-books'),
 ]
--- Book/views.py
+++ Book/views.py
@@ -2,12 +2,18 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from Review.models import Review
 from .models import Book, Genre, GenreBook
 from .serializers import BookSerializer
+
+
+
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
@@ -29,3 +35,20 @@ class BooksByGenreView(APIView):
         books = [genre_book.book for genre_book in genre_books]
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class RecommendedBooksView(APIView):
+   permission_classes = [IsAuthenticated]
+
+   def get(self, request):
+     reviews = Review.objects.filter(user=user, rating__gte=4, rating__lte=5)
+     user = request.user
+     if reviews.count() == 0:
+         return Response({"error": "Not enough info"}, status=400)
+     reviewed_books = [review.book for review in reviews]
+     genre_books = GenreBook.objects.filter(book__in=reviewed_books)
+     genres = [genre_book.genre for genre_book in genre_books]
+     recommended_books = Book.objects.filter(genrebook__genre__in=genres).exclude(id__in=[book.id for book in reviewed_books]).distinct()
+     serializer = BookSerializer(recommended_books, many=True)
+     return Response(serializer.data, status=200)
+
